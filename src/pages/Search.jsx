e00eed1c9290a8acdf79677e86bf23fc6fcfbce8
Pedro Resende <pedroluizrnunez@gmail.com>
2022-11-03 17:58:17 -0300

    requisito 5 completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  state = {
+    isDisabledButtton: true,
+    searchInput: '',
+  };
+
+  handleChangle = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      if (value.length >= 2) {
+        this.setState({
+          isDisabledButtton: false,
+        });
+      } else {
+        this.setState({
+          isDisabledButtton: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { isDisabledButtton, searchInput } = this.state;
     return (
       <div data-testid="page-search" className="search">
         <Header />
-        <h1>Search</h1>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="searchInput"
+            value={ searchInput }
+            onChange={ this.handleChangle }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabledButtton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
